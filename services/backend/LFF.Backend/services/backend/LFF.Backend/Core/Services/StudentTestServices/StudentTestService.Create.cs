8dc06650dae729a13f1c47db472ec1b1af9fb25b
Thim7 <93660448+Thim7@users.noreply.github.com>
2023-11-05 16:14:39 +0700

    Fix (backend): StudentTestService_Create
    Có id student và id bài kiểm tra trả về cho nó 1 bài ktra

--- services/backend/LFF.Backend/services/backend/LFF.Backend/Core/Services/StudentTestServices/StudentTestService.Create.cs
+++ services/backend/LFF.Backend/services/backend/LFF.Backend/Core/Services/StudentTestServices/StudentTestService.Create.cs
@@ -2,8 +2,8 @@ using LFF.Core.Base;
 using LFF.Core.DTOs.StudentTests.Requests;
 using LFF.Core.DTOs.StudentTests.Responses;
 using LFF.Core.Entities;
+using System;
 using System.Threading.Tasks;
-
 namespace LFF.Core.Services.StudentTestServices
 {
     public partial class StudentTestService
@@ -16,14 +16,11 @@ namespace LFF.Core.Services.StudentTestServices
             var entity = new StudentTest();
             entity.StudentId = model.StudentId;
             entity.TestId = model.TestId;
-            entity.StartDate = model.StartDate;
+            entity.StartDate = DateTime.Now;
 
-            //Validation
-            if (model.StartDate is null)
-            {
-                throw BaseDomainException.BadRequest("thời điểm bắt đầu làm bài không được trống");
-            }
+            //Validation: sắp xếp theo độ ưu tiên người dùng
 
+            //Validate dữ liệu
             if (!await userRepository.CheckUserExistedByIdAsync(model.StudentId))
             {
                 throw BaseDomainException.BadRequest($"không tồn tại người dùng nào với id = {model.StudentId}");
@@ -32,10 +29,33 @@ namespace LFF.Core.Services.StudentTestServices
             {
                 throw BaseDomainException.BadRequest($"không tồn tại bài kiểm tra nào với id = {model.TestId}");
             }
+            if ((await userRepository.GetUserByIdAsync(model.StudentId)).Role != UserRoles.Student)
+            {
+                throw BaseDomainException.BadRequest("Chỉ có học viên mới có thể làm bài kiểm tra");
+            }
 
+            //Validate có đang kiểm tra gì không?
+            if (await studentTestRepository.IsDoingAnyTest(model.StudentId))
+                throw BaseDomainException.BadRequest("Bạn đang có một bài kiểm tra chưa hoàn tất");
+
+            //Validate thời gian làm bài
+            var currentDateTime = DateTime.Now;
+            var currentTest = await testRepository.GetByIdAsync(model.TestId);
+
+            if (currentDateTime < currentTest.StartDate || currentTest.EndDate < currentDateTime)
+                throw BaseDomainException.BadRequest("Hiện tại không trong thời gian được phép làm bài kiểm tra");
+
+            //Validate số lần làm bài
+            if (currentTest.NumberOfAttempts >= 0)
+            {
+                int numberOfTimes = await studentTestRepository.NumberOfTimesAttemptTest(model.StudentId, model.TestId);
+                if (numberOfTimes + 1 > currentTest.NumberOfAttempts)
+                    throw BaseDomainException.BadRequest($"Bài kiểm tra này chỉ có thể làm tối đa {currentTest.NumberOfAttempts} lần");
+            }
 
             //Save
             await studentTestRepository.CreateAsync(entity);
+
             //Log
             return await Task.FromResult(new CreateStudentTestResponse(entity));
         }
