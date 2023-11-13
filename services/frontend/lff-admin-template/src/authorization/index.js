76f4b46a0c86efe9688e51c3184f5bdfee400683
Thim7 <93660448+Thim7@users.noreply.github.com>
2023-11-13 21:37:57 +0700

    feat (frontend): Add authorization for Admin template
    authorization with four role: admin, staff, student, teacher

---
+++ services/frontend/lff-admin-template/src/authorization/index.js
@@ -0,0 +1,33 @@
+const getAccessToken = () => {
+  try {
+    const ACCESS_TOKEN = JSON.parse(localStorage.getItem('USER_DATA')).accessToken
+    return ACCESS_TOKEN
+  }
+  catch (error) {
+    return null;
+  }
+}
+
+const ROLE_ADMIN = "ADMIN"
+const ROLE_STAFF = "STAFF"
+const ROLE_STUDENT = "STUDENT"
+const ROLE_TEACHER = "TEACHER"
+
+const ROLES = [ROLE_ADMIN, ROLE_STAFF, ROLE_STUDENT, ROLE_TEACHER]
+
+const getCurrentUserRole = () => {
+  try {
+    const USER_ROLE = JSON.parse(localStorage.getItem('USER_DATA')).data.role
+    if (!ROLES.includes(USER_ROLE))
+      return null
+    return USER_ROLE
+  }
+  catch (error) {
+    return null
+  }
+}
+
+export {
+  getAccessToken, getCurrentUserRole,
+  ROLE_ADMIN, ROLE_STAFF, ROLE_STUDENT, ROLE_TEACHER
+}
