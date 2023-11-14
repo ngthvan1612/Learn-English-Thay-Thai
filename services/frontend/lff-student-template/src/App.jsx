a4c5aee25ca8267ff07dcb2900aa2887d06e7ef9
baongoc02 <tranthibaongoc779152@gmail.com>
2023-11-14 21:35:41 +0700

    update

--- services/frontend/lff-student-template/src/App.jsx
+++ services/frontend/lff-student-template/src/App.jsx
@@ -1,10 +1,15 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
+import StudentLayout from "./pages/StudentLayout"
+import StudentInfoPage from "./pages/StudentInfoPage"
 
 export default function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
+          <Route path="/student" exact="true" element={<StudentLayout/>}>
+            <Route exact="true" index element={<StudentInfoPage/>}/>
+          </Route>
         </Routes>
       </BrowserRouter>
     </>
