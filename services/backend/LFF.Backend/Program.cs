3bec8be443b7049429a1a13387a0987f39935a2d
ngthvan1612 <ngthvan1612@gmail.com>
2023-11-29 00:46:04 +0700

    Update Program.cs

--- services/backend/LFF.Backend/Program.cs
+++ services/backend/LFF.Backend/Program.cs
@@ -3,18 +3,20 @@ using Microsoft.Extensions.Hosting;
 
 namespace LFF.API
 {
-    public class Program
+  public class Program
+  {
+    public static void Main(string[] args)
     {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
+      AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
+      CreateHostBuilder(args).Build().Run();
     }
+
+    public static IHostBuilder CreateHostBuilder(string[] args) =>
+        Host.CreateDefaultBuilder(args)
+            .ConfigureWebHostDefaults(webBuilder =>
+            {
+              webBuilder.UseStartup<Startup>();
+            });
+  }
 }
